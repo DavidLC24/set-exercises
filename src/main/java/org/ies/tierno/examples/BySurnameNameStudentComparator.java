581116f02ae6cc7f4aa249191c7ddb01b67f83c7
david <diurno@B22-B4.lan.tierno.es>
2026-01-29 12:18:10 +0100

    Example

---
+++ src/main/java/org/ies/tierno/examples/BySurnameNameStudentComparator.java
@@ -0,0 +1,17 @@
+package org.ies.tierno.examples;
+
+import java.util.Comparator;
+
+public class BySurnameNameStudentComparator implements Comparator <Student> {
+    @Override
+    public int compare(Student st1, Student st2) {
+        int compare = st1.getSurname().compareTo(st2.getSurname());
+        if(compare == 0) {
+            compare = st1.getName().compareTo(st2.getName());
+            if(compare == 0) {
+                compare = st1.getNif().compareTo(st2.getNif());
+            }
+        }
+        return compare;
+    }
+}
