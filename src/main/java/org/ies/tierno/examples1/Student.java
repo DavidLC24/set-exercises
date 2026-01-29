279fcbcda6b3eec3398686b4ac082cfebdcebc9e
david <diurno@B22-B4.lan.tierno.es>
2026-01-29 12:42:01 +0100

    Example

---
+++ src/main/java/org/ies/tierno/examples1/Student.java
@@ -0,0 +1,26 @@
+package org.ies.tierno.examples1;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Student implements Comparable<Student>{
+        private String nif;
+        private String name;
+        private String surname;
+
+        @Override
+        public int compareTo(Student o) {
+            int compare = this.surname.compareTo(o.getSurname());
+            if (compare == 0) {
+                compare = this.name.compareTo(o.name);
+                if(compare == 0) {
+                    compare = this.nif.compareTo(o.nif);
+                }
+            }
+            return compare;
+        }
+
+    }
+
